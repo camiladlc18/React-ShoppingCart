2401530b163030da7832cb512f98cf8b95154ea9
camiladlc95 <camiladlc95@gmail.com>
2024-09-14 03:00:43 -0400

    Adding the shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
+
 const App = () => {
   return (
  
@@ -10,6 +11,7 @@ const App = () => {
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
+      
       </div>
 
   );
